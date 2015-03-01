a3a2207d41b306312a27611584940b7dad8bc957
BangKindo <gasper.sustar@gmail.com>
2015-03-01 21:16:56 +0100

    Test for remoting a new class to beginner pack; day 2!

---
+++ beginner/src/beginner/TestToday.java
@@ -0,0 +1,12 @@
+package beginner;
+
+public class TestToday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test for new class remoting, day 2!");
+
+	}
+
+}
