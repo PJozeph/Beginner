b80440eec13f6b4e05c4ab5f276c1d40ccb439fb
ELiTE <ELiTE@ELiTE-PC>
2015-03-02 19:36:23 +0100

    Test Commit

---
+++ beginner/src/Theory/fromGitToEclipse.java
@@ -0,0 +1,24 @@
+package Theory;
+
+/*
+ * How to merge Git & Eclipse in that order
+ */
+public class fromGitToEclipse {
+	public static void main(String args[]) {
+
+		System.out.println("LOOK THE COMMENTS!");
+	}
+}
+
+/*
+ * Git repository is made in GitHub(fromEclipse)!
+ * - in eclipse WorkBench Package Explorer click right C 
+ * - import + Git + Project from Git + clone URI
+ * - copy the URI link from GitHub + finish + next
+ * - change Directory to fav location
+ * - import as general project + finish
+ * - DEBUG PROBLEM RESOLVING
+ * - delete Beginner project
+ * - create new java project
+ * - copy the location of Beginner folder in Local Repository folder
+ */
