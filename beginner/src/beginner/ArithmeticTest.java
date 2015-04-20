1b5b2077ca6973faa1420c3bd9ae95ca0d6e39b5
BangKindo <gasper.sustar@gmail.com>
2015-04-20 13:58:58 +0200

    update

--- beginner/src/beginner/ArithmeticTest.java
+++ beginner/src/beginner/ArithmeticTest.java
@@ -8,29 +8,31 @@ public class ArithmeticTest {
 		int number1 = 98;			// Declare an int variable number1 and initialize it to 98
 		int number2 = 5;			// Declare an int variable number2 and initialize it to 5
 		int sum, difference, product, quotient, remainder;		// Declare five int variables to hold result
+		int number3 = 77;
 		
 		// Perform arithmetic Operations
-		sum = number1 + number2;
-		product = number1 * number2;
+		sum = number1 + number2 + number3;
+		product = number1 * number2 * number3;
 		difference = number1 - number2;
 		quotient = number1  / number2;
 		remainder = number1 % number2;
 		
 		// Print results
-		System.out.print("The sum, difference, product, quotient and remainder of ");	// Print description
-		System.out.print(number1);	// Print the value of the variable
-		System.out.print(" and ");
-		System.out.print(number2);
-		System.out.print(" are ");
-		System.out.print(sum);
-		System.out.print(", ");
-		System.out.print(difference);
-		System.out.print(", ");
-		System.out.print(product);
-		System.out.print(", ");
-		System.out.print(quotient);
-		System.out.print(", and ");
-		System.out.println(remainder);
+		System.out.println("The sum, difference, product, quotient and remainder of " + number1 + ", " + number2 + " and " + number3 + " are " + sum +
+				", " + difference + ", " + product + ", " + quotient + ", and " + remainder + ".");	// Print description
+		//System.out.print(number1);	// Print the value of the variable
+		//System.out.print(" and ");
+		//System.out.print(number2);
+		//System.out.print(" are ");
+		//System.out.print(sum);
+		//System.out.print(", ");
+		//System.out.print(difference);
+		//System.out.print(", ");
+		//System.out.print(product);
+		//System.out.print(", ");
+		//System.out.print(quotient);
+		//System.out.print(", and ");
+		//System.out.println(remainder);
 		
 		++number1;		// Increment the value stored in the variable "number1" by 1
 						// Same as "number1 = number1 + 1"
