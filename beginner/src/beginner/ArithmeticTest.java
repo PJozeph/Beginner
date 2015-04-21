c25e96445612eee119b1cd4ade28f7cd7e82d8f5
BangKindo <gasper.sustar@gmail.com>
2015-04-21 13:19:55 +0200

    update

--- beginner/src/beginner/ArithmeticTest.java
+++ beginner/src/beginner/ArithmeticTest.java
@@ -5,21 +5,32 @@ public class ArithmeticTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int number1 = 98;			// Declare an int variable number1 and initialize it to 98
-		int number2 = 5;			// Declare an int variable number2 and initialize it to 5
+		int number1 = 98;	// sum 31 time		// Declare an int variable number1 and initialize it to 98
+		int number2 = 5;	// sum 17 time		// Declare an int variable number2 and initialize it to 5
 		int sum, difference, product, quotient, remainder;		// Declare five int variables to hold result
-		int number3 = 77;
+		int number3 = 77;	// sum 87 time
 		
+		int number4 = 31; 
+		int number5 = 17;
+		int number6 = 87;
+		
+		int sum1 = number1 * number4;
+		int sum2 = number2 * number5;
+		int sum3 = number3 * number6;
+		
+		System.out.println("Product of number1 with 31 is " + sum1);
+		System.out.println("Product of number2 with 17 is " + sum2);
+		System.out.println("Product of number3 with 87 is " + sum3);
 		// Perform arithmetic Operations
-		sum = number1 + number2 + number3;
-		product = number1 * number2 * number3;
-		difference = number1 - number2;
-		quotient = number1  / number2;
-		remainder = number1 % number2;
+		//sum = number1 + number2 + number3;
+		//product = number1 * number2 * number3;
+		//difference = number1 - number2;
+		//quotient = number1  / number2;
+		//remainder = number1 % number2;
 		
 		// Print results
-		System.out.println("The sum, difference, product, quotient and remainder of " + number1 + ", " + number2 + " and " + number3 + " are " + sum +
-				", " + difference + ", " + product + ", " + quotient + ", and " + remainder + ".");	// Print description
+		//System.out.println("The sum, difference, product, quotient and remainder of " + number1 + ", " + number2 + " and " + number3 + " are " + sum +
+			//	", " + difference + ", " + product + ", " + quotient + ", and " + remainder + ".");	// Print description
 		//System.out.print(number1);	// Print the value of the variable
 		//System.out.print(" and ");
 		//System.out.print(number2);
@@ -34,15 +45,15 @@ public class ArithmeticTest {
 		//System.out.print(", and ");
 		//System.out.println(remainder);
 		
-		++number1;		// Increment the value stored in the variable "number1" by 1
+		//++number1;		// Increment the value stored in the variable "number1" by 1
 						// Same as "number1 = number1 + 1"
-		--number2;		// Decrement the value stored in the variable "number2" by 1
+		//--number2;		// Decrement the value stored in the variable "number2" by 1
 						// Same as "number2 = number2 -1"
-		System.out.println("number1 after increment is " + number1);	// Print description and variable
-		System.out.println("number2 after decrement is " + number2);
-		quotient = number1 / number2;
-		System.out.println("The new quotiont of " + number1 +  " and " + number2
-				+ " is " + quotient);
+		//System.out.println("number1 after increment is " + number1);	// Print description and variable
+		//System.out.println("number2 after decrement is " + number2);
+		//quotient = number1 / number2;
+		//System.out.println("The new quotiont of " + number1 +  " and " + number2
+			//	+ " is " + quotient);
 
 	}
 
