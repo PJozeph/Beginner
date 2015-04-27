12abfe28bd6c21deba18a3b97295868b24dd5924
Giboo <Giboo@Giboo-PC>
2015-04-27 16:34:05 +0200

    ..

--- beginner/src/beginner/RunningNumberSum.java
+++ beginner/src/beginner/RunningNumberSum.java
@@ -13,43 +13,16 @@ public class RunningNumberSum {
 		
 		// use a while loop to  repeatitively sum from  the lowerbound to the upperbound 
 		int number = lowerbound;
-		//number = number + 2;
-		// zero:
-		/*
-		 * while (number <= upperbound) {
-			 		sum=+number;
-			 		++number;
-			 	}
-		 */	
-	
-		// first:
-		/*
-		 * int lowerbound = 9;
-		 * int upperbound = 888;
-		 * int sum = 0;
-		 */
+		//number = number +2;
 		
-		// second:
-		while (number<=upperbound) {
-			sum = sum+number;
-			number = number/2;
-			//++number;
-					//++number;
-		}
-		
-		
-		// third: 
-		/*
-		 * while (number <=upperbound) {
-			if(number%7==0){
-				sum = sum + number;
-				++number;
-			}
-					// Accumulate number into sum
-							// Next number
+		// zero:		
+		while (number <= upperbound) {
+			sum=+number;
 			++number;
-		} 
-		 */
+		}
+		 	
+	
+		
 		
 		
 		System.out.println("The sum from " + lowerbound + " to " + upperbound + " is " + sum);
@@ -58,9 +31,5 @@ public class RunningNumberSum {
 }
 
 
-// first: Modify the above program to sum all the numbers from 9 to 888. (Ans: 394680.)
-// second: Modify the above program to sum all the odd numbers between 1 to 1000. (Hint: Change the post-processing statement to "number = number + 2". Ans: 250000)
-// third: Modify the above program to sum all the numbers between 1 to 1000 that are divisible by 7. (Hint: Modify the initialization and post-processing statements. Ans: 71071.)
-// fourth: Modify the above program to find the sum of the square of all the numbers from 1 to 100, i.e. 1*1 + 2*2 + 3*3 +... (Ans: 338350.)
-// fifth: Modify the above program (called RunningNumberProduct) to compute the product of all the numbers from 1 to 10. (Hint: Use a variable called product instead of sum and initialize product to 1. Ans: 3628800.)
+
 
